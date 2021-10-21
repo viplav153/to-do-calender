125a4ea938bfa4db6855e24223a5abf9b97137ff
viplav153 <42774286+viplav153@users.noreply.github.com>
2021-10-21 20:08:23 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,44 +6,7 @@ import numpy as np
 import random
 from time import sleep
 
-# Creates an empty board
-def create_board():
-	return(np.array([[0, 0, 0],
-					[0, 0, 0],
-					[0, 0, 0]]))
 
-# Check for empty places on board
-def possibilities(board):
-	l = []
-	
-	for i in range(len(board)):
-		for j in range(len(board)):
-			
-			if board[i][j] == 0:
-				l.append((i, j))
-	return(l)
-
-# Select a random place for the player
-def random_place(board, player):
-	selection = possibilities(board)
-	current_loc = random.choice(selection)
-	board[current_loc] = player
-	return(board)
-
-# Checks whether the player has three
-# of their marks in a horizontal row
-def row_win(board, player):
-	for x in range(len(board)):
-		win = True
-		
-		for y in range(len(board)):
-			if board[x, y] != player:
-				win = False
-				continue
-				
-		if win == True:
-			return(win)
-	return(win)
 
 # Checks whether the player has three
 # of their marks in a vertical row
