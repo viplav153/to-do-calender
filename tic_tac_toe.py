da163c85a37df174bba74eaa4d16ef3762541b88
viplav153 <42774286+viplav153@users.noreply.github.com>
2021-10-22 13:54:40 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,33 +1,4 @@
-# Tic-Tac-Toe Program using
-# random number in Python
 
-# importing all necessary libraries
-import numpy as np
-import random
-from time import sleep
-# Creates an empty board
-def create_board():
-    return(np.array([[0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]]))
-
-# Check for empty places on board
-def possibilities(board):
-    l = []
-
-    for i in range(len(board)):
-        for j in range(len(board)):
-
-            if board[i][j] == 0:
-                l.append((i, j))
-    return(l)
-
-# Select a random place for the player
-def random_place(board, player):
-    selection = possibilities(board)
-    current_loc = random.choice(selection)
-    board[current_loc] = player
-    return(board)
 
 # Checks whether the player has three
 # of their marks in a horizontal row
